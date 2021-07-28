f8dec4a9f139b0406040148c218a3342af2617a7
David Hormiga Afonso <davidhormigafonso@gmail.com>
2021-07-28 16:08:43 +0100

    All tests approved, now it's time to refactor.

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,41 @@
-function clasificaElementos(index) {
-    return [0,0,0];
-}
+function clasificaElementos(arrayValue) {
+
+  numbers = []
+
+  for(let i=0; i<arrayValue.length; i++){
+      numbers.push(arrayValue[i])
+  }
+
+  console.log(numbers)
+  console.log(numbers.length)
+
+  positiveNumbers = []
+  negativeNumbers = []
+  zeroNumbers = []
+
+  for(let i=0; i<numbers.length; i++){
+    if(Math.sign(numbers[i]) == 1){
+        positiveNumbers.push(numbers[i])
+    }
+    if(Math.sign(numbers[i]) == -1){
+      negativeNumbers.push(numbers[i])
+    }
+    if(Math.sign(numbers[i]) == 0){
+      zeroNumbers.push(numbers[i])
+    }
+  }
+
+  console.log(positiveNumbers)
+
+  let allNumberSized = [
+    (positiveNumbers.length / numbers.length).toFixed(4),
+    (negativeNumbers.length / numbers.length).toFixed(4),
+    (zeroNumbers.length / numbers.length).toFixed(4),
+  ];
+
+  return allNumberSized;
+};
+
+console.log(clasificaElementos([0,0,1,-1,2]))
+
 module.exports = clasificaElementos;
