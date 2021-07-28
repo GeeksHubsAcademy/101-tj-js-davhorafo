51ff1e84962f29bffcd984f1ac0621a3bec43b47
David Hormiga Afonso <davidhormigafonso@gmail.com>
2021-07-28 16:13:34 +0100

    All refactored, all tests passed

--- test/test01.js
+++ test/test01.js
@@ -1,41 +1,33 @@
 function clasificaElementos(arrayValue) {
+  allNumbers = [];
+  positiveNumbers = [];
+  negativeNumbers = [];
+  zeroNumbers = [];
 
-  numbers = []
-
-  for(let i=0; i<arrayValue.length; i++){
-      numbers.push(arrayValue[i])
+  for (let i = 0; i < arrayValue.length; i++) {
+    allNumbers.push(arrayValue[i]);
   }
 
-  console.log(numbers)
-  console.log(numbers.length)
-
-  positiveNumbers = []
-  negativeNumbers = []
-  zeroNumbers = []
-
-  for(let i=0; i<numbers.length; i++){
-    if(Math.sign(numbers[i]) == 1){
-        positiveNumbers.push(numbers[i])
+  for (let i = 0; i < allNumbers.length; i++) {
+    const whichIsYourSign = Math.sign(allNumbers[i]);
+    if (whichIsYourSign == 1) {
+      positiveNumbers.push(allNumbers[i]);
     }
-    if(Math.sign(numbers[i]) == -1){
-      negativeNumbers.push(numbers[i])
+    if (whichIsYourSign == -1) {
+      negativeNumbers.push(allNumbers[i]);
     }
-    if(Math.sign(numbers[i]) == 0){
-      zeroNumbers.push(numbers[i])
+    if (whichIsYourSign == 0) {
+      zeroNumbers.push(allNumbers[i]);
     }
   }
 
-  console.log(positiveNumbers)
-
-  let allNumberSized = [
-    (positiveNumbers.length / numbers.length).toFixed(4),
-    (negativeNumbers.length / numbers.length).toFixed(4),
-    (zeroNumbers.length / numbers.length).toFixed(4),
+  let allNumbersMeasured = [
+    (positiveNumbers.length / allNumbers.length).toFixed(4),
+    (negativeNumbers.length / allNumbers.length).toFixed(4),
+    (zeroNumbers.length / allNumbers.length).toFixed(4),
   ];
 
-  return allNumberSized;
-};
-
-console.log(clasificaElementos([0,0,1,-1,2]))
+  return allNumbersMeasured;
+}
 
 module.exports = clasificaElementos;
